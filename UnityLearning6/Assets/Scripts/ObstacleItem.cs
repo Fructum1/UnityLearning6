dd202585cf08dd5d1217b1f4641baf8e8a90aed9
Fructum1 <79308915+Fructum1@users.noreply.github.com>
2022-04-28 23:41:05 +0700

    Squashed commit of the following:
    commit 7bd92d6f484fe74552a00ac1e3e13e042c571716
    Author: Fructum1 <79308915+Fructum1@users.noreply.github.com>
    Date:   Thu Apr 28 23:27:37 2022 +0700
    
        доделал
    
    commit b83fb4978c3e6b6f201fc77770f6dd40945ff2b8
    Author: annadorer <78897263+annadorer@users.noreply.github.com>
    Date:   Thu Apr 28 00:29:54 2022 +0700
    
        победа

--- UnityLearning6/Assets/Scripts/ObstacleItem.cs
+++ UnityLearning6/Assets/Scripts/ObstacleItem.cs
@@ -1,21 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ObstacleItem : MonoBehaviour
 {
+    private float currentValue=1;
+    private UnityEvent onDestroyObstacle;
+    [SerializeField]
+    private Material m_material;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_material = GetComponent<Renderer>().material;
     }
-    public void GetDamage(float value)
+	
+public void GetDamage(float value) 
     {
+        currentValue -= value;
+        if (currentValue == 1)
+        {
+            m_material.color = Color.white;
+        }
 
+        if (currentValue <= 0 && transform.GetComponent<Renderer>().material.color == Color.red)
+        {
+            onDestroyObstacle?.Invoke();
+            Destroy(gameObject);
+        }
     }
+
     // Update is called once per frame
     void Update()
-    {
-        
+    {   
+        if (currentValue <= 0)
+        {
+            transform.GetComponent<Renderer>().material.color = Color.Lerp(Color.white, Color.red, Time.time / 10);
+        }
+
     }
 }
